91c88b7e5e7c49f0f03d768d950daa7f261fc9bf
Gabriel Witor <witor.alm@gmail.com>
2025-12-30 07:28:35 -0300

    chore: add fields validations

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,12 +1,19 @@
-﻿namespace LibraryManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManager.Models
 {
     public class Book
     {
         public int BookId { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Author { get; set; }
+        [Required]
         public string Genre { get; set; }
+        [Range(0,100000d)]
         public double Price { get; set; }
-        public double Stock { get; set; }
+        [Range(0, 999)]
+        public int Stock { get; set; }
     }
 }
